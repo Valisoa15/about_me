d374c6194750cdc386fadf978ac38fff58cf72f3
Valisoa15 <lecourageuxleva@gmail.com>
2021-07-09 17:54:56 +0300

    react

---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../styles/Page.css';
+
+const Page = () => {
+    return (
+        <div>
+            <h1 className="az">teqt io</h1>
+        </div>
+    );
+};
+
+export default Page;
